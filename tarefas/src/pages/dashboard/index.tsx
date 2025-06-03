8328d6ccf0d9e98410edbdec5b2e981ab956a564
basilioruan <ruan_basilio@outlook.com>
2025-06-03 19:03:36 -0300

    wip: initial create of dashboard page

---
+++ tarefas/src/pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from './styles.module.css';
+
+export default function Dashboard() {
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title> Meu painel de tarefas </title>
+      </Head>
+
+      <h1> Página Painel </h1>
+    </div>
+  )
+
+}
